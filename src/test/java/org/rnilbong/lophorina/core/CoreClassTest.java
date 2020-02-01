5073d4ccb51cf0d30d378402a686c0a7071e75be
jinyeong <jinyp91@gmail.com>
2020-02-01 18:19:35 +0900

    init project
    - set package
    - set gradle build
    - add sample code
    - edit gitignore

---
+++ src/test/java/org/rnilbong/lophorina/core/CoreClassTest.java
@@ -0,0 +1,12 @@
+package org.rnilbong.lophorina.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoreClassTest {
+
+    @Test
+    void run() {
+    }
+}
