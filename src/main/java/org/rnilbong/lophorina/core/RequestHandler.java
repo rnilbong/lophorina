f37b1b0236fb39e5a8c49bd9eabcca1e65679bab
jinyeong <jinyp91@gmail.com>
2020-02-20 19:35:39 +0900

    불필요한 코드제거 http 메서드 추가

--- src/main/java/org/rnilbong/lophorina/core/RequestHandler.java
+++ src/main/java/org/rnilbong/lophorina/core/RequestHandler.java
@@ -1,12 +1,8 @@
 package org.rnilbong.lophorina.core;
 
-import com.google.gson.Gson;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.util.EntityUtils;
 import org.rnilbong.lophorina.utils.HttpUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,19 +15,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-/*
- * todo
- * 1. target server api 에 다양한 api 추가
- *     1. 여러가지 응답 확인
- *         1. 현재 json 타입이 클라이언트로 전달 안됨
- *     1. 여러가지 메서드 확인
- *         1. 현재 post, get 확인
- * 1. 404 등 에러 처리
- * 1. 코어기능 추가
- * 1. 코어 코드 리펙토링
- * 1. 테스트 코드
- */
-
 public class RequestHandler extends Thread {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private static final String DEFAULT_FILE_PATH = "images/Lophorina.jpg";
@@ -41,33 +24,25 @@ public class RequestHandler extends Thread {
 
     private static final String PORT_DELIMITER = ":";
 
-    private Gson gson;
     private Socket connectionSocket;
 
     public RequestHandler(Socket connectionSocket) {
         this.connectionSocket = connectionSocket;
-        this.gson = new Gson();
     }
 
     @Override
     public void run() {
         logger.debug("WebServer Thread Created");
-//        BufferedWriter outToClient;
         Map<String, String> headerMap = new HashMap<>();
 
         try (BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-        ) {
-//            outToClient = new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
-
-            //head
+             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream())) {
+            //http call head
             String requestMessageLine = inFromClient.readLine();
             if (requestMessageLine == null) {
                 return;
             }
             logger.debug("head: " + requestMessageLine);
-//            String[] headSplitStr = requestMessageLine.split(": ");
-//            headerMap.put(headSplitStr[0], headSplitStr[1]);
 
             //request header
             String headerLine;
@@ -90,27 +65,38 @@ public class RequestHandler extends Thread {
             String requestUrl;
 
             HttpResponse httpResponse = null;
-            if (requestMethod.equals("GET")) {
-                requestUrl = tokenizedLine.nextToken();
-
-                httpResponse = HttpUtil.get(getPassUrl() + requestUrl, headerMap);
-                logger.debug(httpResponse.toString());
-            } else if (requestMethod.equals("POST")) {
-                requestUrl = tokenizedLine.nextToken();
-
-                httpResponse = HttpUtil.post(getPassUrl() + requestUrl, payload.toString(), headerMap);
-                logger.debug(httpResponse.toString());
-            } else {
-                outToClient.writeBytes("HTTP/1.1 400 Bad Request Message \r\n");
-                outToClient.writeBytes("Connection: close\r\n");
-                outToClient.writeBytes("\r\n");
-                logger.error("Bad Request");
+            switch (requestMethod) {
+                case "GET":
+                    requestUrl = tokenizedLine.nextToken();
+                    httpResponse = HttpUtil.get(getPassUrl() + requestUrl, headerMap);
+                    logger.debug(httpResponse.toString());
+                    break;
+                case "POST":
+                    requestUrl = tokenizedLine.nextToken();
+                    httpResponse = HttpUtil.post(getPassUrl() + requestUrl, payload.toString(), headerMap);
+                    logger.debug(httpResponse.toString());
+                    break;
+                case "PUT":
+                    requestUrl = tokenizedLine.nextToken();
+                    httpResponse = HttpUtil.put(getPassUrl() + requestUrl, payload.toString(), headerMap);
+                    logger.debug(httpResponse.toString());
+                    break;
+                case "DELETE":
+                    requestUrl = tokenizedLine.nextToken();
+                    httpResponse = HttpUtil.delete(getPassUrl() + requestUrl, headerMap);
+                    logger.debug(httpResponse.toString());
+                    break;
+                default:
+                    outToClient.writeBytes("HTTP/1.1 400 Bad Request\r\n");
+                    outToClient.writeBytes("Connection: close\r\n");
+                    outToClient.writeBytes("\r\n");
+                    logger.error("Bad Request");
+                    break;
             }
 
             if (httpResponse != null) {
                 sendResponse(httpResponse, outToClient);
             }
-//            sendResponseDefaultImage(requestUrl, outToClient);
 
             connectionSocket.close();
             logger.debug("Connection Closed");
@@ -134,10 +120,8 @@ public class RequestHandler extends Thread {
 
         Arrays.stream(httpResponse.getAllHeaders())
                 .filter(header -> !header.getName().equals("Transfer-Encoding"))
-                .forEach(header -> {
-                    outputHeader.append(header.toString())
-                                .append("\r\n");
-                });
+                .forEach(header -> outputHeader.append(header.toString())
+                        .append("\r\n"));
 
         String body = "";
         if (String.valueOf(httpResponse.getStatusLine().getStatusCode()).startsWith("2")) {
@@ -146,7 +130,7 @@ public class RequestHandler extends Thread {
 
         }
 
-        if(httpResponse.getEntity().isChunked()) { //Chunk 데이터이면 length 추가
+        if (httpResponse.getEntity().isChunked()) { //Chunk 데이터이면 length 추가
             outputHeader.append("Content-Length: ")
                     .append(body.length())
                     .append("\r\n");
@@ -158,7 +142,6 @@ public class RequestHandler extends Thread {
         outToClient.writeBytes(body);
 
         outToClient.flush();
-//        outToClient.close();
     }
 
     private void sendResponseDefaultImage(String requestUrl, DataOutputStream outToClient) throws IOException {
--- src/main/java/org/rnilbong/lophorina/utils/HttpUtil.java
+++ src/main/java/org/rnilbong/lophorina/utils/HttpUtil.java
@@ -2,8 +2,10 @@ package org.rnilbong.lophorina.utils;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
@@ -19,21 +21,10 @@ public class HttpUtil {
         try {
             HttpClient client = HttpClientBuilder.create().build();
             HttpGet getRequest = new HttpGet(requestURL);
-
             for (String key : headerMap.keySet()) {
                 getRequest.addHeader(key, headerMap.get(key));
             }
-
             response = client.execute(getRequest);
-
-//            if (response.getStatusLine().getStatusCode() == 200) {
-//                ResponseHandler<String> handler = new BasicResponseHandler();
-//                String body = handler.handleResponse(response);
-//                logger.debug(body);
-//            } else {
-//                logger.error("response is error : " + response.getStatusLine().getStatusCode());
-//            }
-
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage());
             throw e;
@@ -42,27 +33,51 @@ public class HttpUtil {
         return response;
     }
 
-    public static HttpResponse post(String requestURL, String jsonMessage, Map<String, String> headerMap) throws Exception {
+    public static HttpResponse post(String requestURL, String body, Map<String, String> headerMap) throws Exception {
         HttpResponse response;
         try {
             HttpClient client = HttpClientBuilder.create().build();
             HttpPost postRequest = new HttpPost(requestURL);
-            postRequest.setHeader("Accept", "application/json");
-            postRequest.setHeader("Connection", "keep-alive");
-            postRequest.setHeader("Content-Type", "application/json");
-            //postRequest.addHeader("Authorization", token);
-
-            postRequest.setEntity(new StringEntity(jsonMessage));
-
+            for (String key : headerMap.keySet()) {
+                postRequest.addHeader(key, headerMap.get(key));
+            }
+            postRequest.setEntity(new StringEntity(body));
             response = client.execute(postRequest);
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
+            throw e;
+        }
 
-//            if (response.getStatusLine().getStatusCode() == 200) {
-//                ResponseHandler<String> handler = new BasicResponseHandler();
-//                String body = handler.handleResponse(response);
-//                logger.debug(body);
-//            } else {
-//                logger.error("response is error : " + response.getStatusLine().getStatusCode());
-//            }
+        return response;
+    }
+
+    public static HttpResponse put(String requestURL, String body, Map<String, String> headerMap) throws Exception {
+        HttpResponse response;
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPut putRequest = new HttpPut(requestURL);
+            for (String key : headerMap.keySet()) {
+                putRequest.addHeader(key, headerMap.get(key));
+            }
+            putRequest.setEntity(new StringEntity(body));
+            response = client.execute(putRequest);
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage());
+            throw e;
+        }
+
+        return response;
+    }
+
+    public static HttpResponse delete(String requestURL, Map<String, String> headerMap) throws Exception {
+        HttpResponse response;
+        try {
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpDelete deleteRequest = new HttpDelete(requestURL);
+            for (String key : headerMap.keySet()) {
+                deleteRequest.addHeader(key, headerMap.get(key));
+            }
+            response = client.execute(deleteRequest);
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage());
             throw e;
